3ac19927e2114ec7e85d5213b71c657323d49d83
Eduard Bodan <bodaneduard@gmail.com>
2026-04-09 10:50:43 +0200

    update

--- src/main.c
+++ src/main.c
@@ -2,13 +2,5 @@
 
 int main(void)
 {
-    DDRB |= (1 << DDB5); // Set pin 13 as output
-
-    while (1)
-    {
-        PORTB ^= (1 << PORTB5); // Toggle pin 13
-        for (volatile uint32_t i = 0; i < 100000; i++); // Delay
-    }
-
-    return 0;
+   
 }
